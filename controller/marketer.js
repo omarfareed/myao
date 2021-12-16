760cdf288db978cccae98f34bd0b93f1187cd283
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2021-12-16 23:57:54 +0200

    Update marketer.js

--- controller/marketer.js
+++ controller/marketer.js
@@ -4,16 +4,17 @@ const controller = require("./globalController");
 
 exports.getMarketers = controller.select("marketer");
 exports.createMarketer = controller.create("marketer");
-exports.updateMarketer = controller.update("marketer", ["id"]);
+exports.updateMarketer = controller.update("marketer", ["id", "founded_at"]); // don't update the id(constant attribute)
+exports.deleteMarketer = controller.delete("marketer");
 exports.searchMarketer = (req, res, next) => {
   let arr = []; //req.body.search.split(" ");
   let q;
 
   if (arr.length == 1)
-    q = `Select * from marketer where Fname like '%${arr[0]}' or Lname like '${arr[0]}'`;
+    q = `Select * from marketer where fname like '%${arr[0]}' or lname like '${arr[0]}'`;
   else
-    q = `Select * from marketer where Fname like '%${arr[0]}' or Lname like '${arr[1]}'`;
-  connection.query(q, (err, data) => {
+    q = `Select * from marketer where fname like '%${arr[0]}' or lname like '${arr[1]}'`;
+  connection.query(q, (err, data) => { 
     if (err)
       return res.json({
         status: "fail",
