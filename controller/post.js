4ce7410f274ed7e658e6c8bf2d37a9ef3d9f5749
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2021-12-16 23:58:27 +0200

    Update post.js

--- controller/post.js
+++ controller/post.js
@@ -2,4 +2,5 @@ const connection = require("../connection");
 const controller = require("./globalController");
 exports.getPosts = controller.select("post");
 exports.createPost = controller.create("post");
-exports.updatePost = controller.update("post", ["id"]);
+exports.updatePost = controller.update("post", ["id", "surfer_id", "created_date"]);
+exports.deletePost = controller.delete("post");
